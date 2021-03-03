faaec46adac0ca1b0659e9f7794b25aafbe71996
김성민 <seongmin.kim@thingsflow.kr>
2021-03-03 22:43:55 +0900

    코인 리스트 불러오기 추가.

--- UpMyCoin/Main/CoinListView/CoinListViewController.swift
+++ UpMyCoin/Main/CoinListView/CoinListViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class CoinListViewController: UIViewController {
+    
+    static func instance() -> CoinListViewController {
+        let vc = CoinListViewController(nibName: CoinListViewController.className, bundle: nil)
+        return vc
+    }
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var viewModel: CoinListViewModel = CoinListViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        viewModel.getCoinList()
     }
     
 }
--- UpMyCoin/Main/CoinListView/CoinListViewModel.swift
+++ UpMyCoin/Main/CoinListView/CoinListViewModel.swift
@@ -6,7 +6,39 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 struct CoinListViewModel {
     
+    let coinItem = BehaviorRelay<[CoinResponse]>(value: [])
+    private let serverUtil = ServerUtil.shared
+    private var disposeBag = DisposeBag()
+    init() {
+        
+    }
+    
+    
+    func getCoinList() {
+        var httpRequest = HttpRequest()
+        httpRequest.url = "market/all?isDetails=false"
+        
+        serverUtil.rx.requestRx(with: httpRequest).subscribe(onNext: { (list: [CoinResponse]) in
+            self.coinItem.accept(list)
+        }).disposed(by: disposeBag)
+
+    }
+}
+
+
+/*
+ "market": "KRW-BTC",
+ "korean_name": "비트코인",
+ "english_name": "Bitcoin"
+ */
+
+struct CoinResponse: Decodable {
+    let market: String
+    let koreanName: String
+    let englishName: String
 }
